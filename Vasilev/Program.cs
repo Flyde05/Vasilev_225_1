e9d2e984eb2557d12af90a668afade0fa1f3ed41
Kristina Popova <kristina_14_09@vk.com>
2023-11-24 12:48:04 +0300

    Добавьте файлы проекта.

---
+++ Vasilev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vasilev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите стороны А и B по очереди: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double S = a * b;
+            double P = 2 * (a + b);
+            Console.WriteLine("Периметр (P) = {0}, Площадь (S) = {1}", P, S);
+        }
+    }
+}
