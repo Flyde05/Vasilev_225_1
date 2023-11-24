ff7b0e245b3dd8c90406708ba174e0e0c4186a12
Kristina Popova <kristina_14_09@vk.com>
2023-11-24 13:43:28 +0300

    segodnya dr

---
+++ Vasilev/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vasilev12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите стороны А и B по очереди: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double S = a * b;
+            double P = 2 * (a + b);
+            Console.WriteLine("Периметр (P) = {0}, Площадь (S) = {1}", P, S);
+        }
+    }
+}
+
+
+
